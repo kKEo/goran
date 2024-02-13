1ad18a0bb129ad19802b51c100c361d28904a741
Krzysztof Maziarz <krzysztof.maziarz@gmail.com>
2024-02-13 22:28:06 +0100

    Reorganise code by moving api handler to separate package

---
+++ webapp/api/user.go
@@ -0,0 +1,41 @@
+package api
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/kkEo/g-mk8s/webapp/model"
+	"gorm.io/gorm"
+	"net/http"
+)
+
+type UserHandlers struct {
+	DB *gorm.DB
+}
+
+func (h *UserHandlers) GetUser(c *gin.Context) {
+	var user model.User
+	name := c.Param("name")
+
+	result := h.DB.First(&user, "name = ?", name)
+
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+func (h *UserHandlers) PostUser(c *gin.Context) {
+	var newUser model.User
+	if err := c.BindJSON(&newUser); err != nil {
+		c.AbortWithStatusJSON(400, gin.H{"error": "Bad request"})
+		return
+	}
+
+	h.DB.Create(&newUser)
+	c.JSON(200, newUser)
+}
--- webapp/main.go
+++ webapp/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"gorm.io/gorm"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kkEo/g-mk8s/webapp/api"
 	"github.com/kkEo/g-mk8s/webapp/db"
 	"github.com/kkEo/g-mk8s/webapp/middleware"
 	"github.com/kkEo/g-mk8s/webapp/model"
@@ -32,43 +31,11 @@ func main() {
 	protected := router.Group("/protected")
 	protected.Use(middleware.AuthMiddleware())
 
-	protected.GET("/greet/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		c.JSON(http.StatusOK, gin.H{
-			"message": fmt.Sprintf("Hello, %s", name),
-		})
-	})
-
-	protected.POST("/users", func(c *gin.Context) {
-		var newUser model.User
-		if err := c.BindJSON(&newUser); err != nil {
-			c.AbortWithStatusJSON(400, gin.H{"error": "Bad request"})
-			return
-		}
-
-		database.Create(&newUser)
-		c.JSON(200, newUser)
-	})
-
-	router.GET("/users/:name", func(c *gin.Context) {
-		var user model.User
-		name := c.Param("name")
-
-		result := database.First(&user, "name = ?", name)
-
-		if result.Error != nil {
-			if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-				c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
-				return
-			}
-			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
-			return
-		}
-		c.JSON(http.StatusOK, user)
-	})
+	userApiHandlers := &api.UserHandlers{DB: database}
+	router.GET("/users/:name", userApiHandlers.GetUser)
+	protected.POST("/users", userApiHandlers.PostUser)
 
 	log.Println("Starting server on :8080")
-
 	router.Run(":8080")
 
 }
